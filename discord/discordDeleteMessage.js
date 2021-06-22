f71436f2c80a62d04c8975e2360502137929382e
markoudstaal <mark_koudstaal@hotmail.com>
2021-06-22 13:57:49 +0200

    Allow either objects or strings as channel and msg

--- discord/discordDeleteMessage.js
+++ discord/discordDeleteMessage.js
@@ -7,11 +7,51 @@ module.exports = function (RED) {
     var configNode = RED.nodes.getNode(config.token);
     discordBotManager.getBot(configNode).then(function (bot) {
       node.on('input', function (msg) {
-        const msgId = msg.id;
-        const channelId = msg.channel;
+        if (!msg.id || !msg.channel) {
+          node.error(`Either msg.id or msg.channel wasn't set`);
+          node.status({
+            fill: "red",
+            shape: "dot",
+            text: `msg.id and msg.channel are required`
+          })
+          return;
+        }
+
+        var msgId = msg.id;
+        if (msgId && typeof msgId !== 'string') {
+          if (msgId.hasOwnProperty('id')) {
+            msgId = msgID.id;
+          } else {
+            msgId = undefined;
+            node.error(`msg.Id needs to be either a string for the id or channel Object`);
+            node.status({
+              fill: "red",
+              shape: "dot",
+              text: `msg.id is not a string or Object`
+            })
+            return;
+          }
+        }
+
+        var channel = msg.channel;
+        if (channel && typeof channel !== 'string') {
+          if (channel.hasOwnProperty('id')) {
+            channel = channel.id;
+          } else {
+            channel = undefined;
+            node.error(`msg.channel needs to be either a string for the id or channel Object`);
+            node.status({
+              fill: "red",
+              shape: "dot",
+              text: `msg.channel is not a string or Object`
+            })
+            return;
+          }
+        }
+
         const timeDelay = msg.timedelay || 0;
 
-        bot.channels.fetch(channelId).then((channelInstance) => {
+        bot.channels.fetch(channel).then((channelInstance) => {
           channelInstance.messages.fetch(msgId).then(message => {
             message.delete({
               timeout: timeDelay
--- discord/discordSendMessage.js
+++ discord/discordSendMessage.js
@@ -10,11 +10,45 @@ module.exports = function (RED) {
     var configNode = RED.nodes.getNode(config.token);
     discordBotManager.getBot(configNode).then(function (bot) {
       node.on('input', function (msg) {
-        var msgId = undefined || msg.id;
+        if (!msg.channel) {
+          node.error(`msg.channel wasn't set`);
+          node.status({
+            fill: "red",
+            shape: "dot",
+            text: `msg.channel is required`
+          })
+          return;
+        }
 
         var channel = config.channel || msg.channel;
-        if (!channel) {
-          channel = undefined;
+        if (channel && typeof channel !== 'string') {
+          if (channel.hasOwnProperty('id')) {
+            channel = channel.id;
+          } else {
+            node.error(`msg.channel needs to be either a string for the id or channel Object`);
+            node.status({
+              fill: "red",
+              shape: "dot",
+              text: `msg.channel is not a string or Object`
+            })
+            return;
+          }
+        }
+
+        var msgId = undefined || msg.id;
+        if (msgId && typeof msgId !== 'string') {
+          if (msgId.hasOwnProperty('id')) {
+            msgId = msgID.id;
+          } else {
+            msgId = undefined;
+            node.error(`msg.id needs to be either a string for the id or message Object`);
+            node.status({
+              fill: "red",
+              shape: "dot",
+              text: `msg.id is not a string or Object`
+            })
+            return;
+          }
         }
 
         let attachment = null;
@@ -22,57 +56,55 @@ module.exports = function (RED) {
           attachment = new MessageAttachment(msg.attachment);
         }
 
-        if (channel) {
-          bot.channels.fetch(channel).then((channelInstance) => {
-            if (msgId) {
-              channelInstance.messages.fetch(msgId).then(message => {
-                message.edit(msg.payload).then(function () {
-                  node.status({
-                    fill: "green",
-                    shape: "dot",
-                    text: "message sent"
-                  });
-                }).catch(function (err) {
-                  node.error("Couldn't edit message:" + err);
-                  node.status({
-                    fill: "red",
-                    shape: "dot",
-                    text: "Error while editing message"
-                  });
-                });
-              }).catch(error => {
-                node.error(`Couldn't find the message: ${error}`);
-                node.status({
-                  fill: "red",
-                  shape: "dot",
-                  text: "Couldn't find supplied message with supplied message ID"
-                });
-              });
-            } else {
-              channelInstance.send(msg.payload, attachment).then(function () {
+        bot.channels.fetch(channel).then((channelInstance) => {
+          if (msgId) {
+            channelInstance.messages.fetch(msgId).then(message => {
+              message.edit(msg.payload).then(function () {
                 node.status({
                   fill: "green",
                   shape: "dot",
                   text: "message sent"
                 });
               }).catch(function (err) {
-                node.error("Couldn't send to channel:" + err);
+                node.error("Couldn't edit message:" + err);
                 node.status({
                   fill: "red",
                   shape: "dot",
-                  text: "send error"
+                  text: "Error while editing message"
                 });
               });
-            };
-          }).catch(error => {
-            node.error(`Couldn't find the supplied channel ID: ${error}`);
-            node.status({
-              fill: "red",
-              shape: "dot",
-              text: `Couldn't find channel with the supplied channel ID...`
+            }).catch(error => {
+              node.error(`Couldn't find the message: ${error}`);
+              node.status({
+                fill: "red",
+                shape: "dot",
+                text: "Couldn't find supplied message with supplied message ID"
+              });
             });
-          })
-        };
+          } else {
+            channelInstance.send(msg.payload, attachment).then(function () {
+              node.status({
+                fill: "green",
+                shape: "dot",
+                text: "message sent"
+              });
+            }).catch(function (err) {
+              node.error("Couldn't send to channel:" + err);
+              node.status({
+                fill: "red",
+                shape: "dot",
+                text: "send error"
+              });
+            });
+          };
+        }).catch(error => {
+          node.error(`Couldn't find the supplied channel ID: ${error}`);
+          node.status({
+            fill: "red",
+            shape: "dot",
+            text: `Couldn't find channel with the supplied channel ID...`
+          });
+        });
 
         node.on('close', function () {
           discordBotManager.closeBot(bot);
